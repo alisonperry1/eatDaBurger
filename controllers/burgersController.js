bf50e91b35c6fd6da8dba7bc067b1bc2b6e59afc
alison <alison.faith.perry@gmail.com>
2017-11-21 15:00:42 -0600

    done

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,40 +1,24 @@
 var express = require("express");
-
 var router = express.Router();
 var burger = require("../models/burger.js");
-
-// get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
+res.redirect("/burgers");
 });
-
 router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-    res.render("index", { burger_data: burgerData });
-  });
+burger.all(function(burgerData) {
+res.render("index", { burger_data: burgerData });
 });
-
-// post route -> back to index
+});
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+burger.create(req.body.burger_name, function(result) {
+console.log(result);
+res.redirect("/");
 });
-
-// put route -> back to index
+});
 router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+burger.update(req.body.burger_id, function(result) {
+console.log(result);
+res.redirect("/");
 });
-
+});
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,9 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgerdb;
+USE burgerdb;
 
-# If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS burgers;
+DROP TABLE IF EXISTS burger;
 
-# Create the burgers table
-CREATE TABLE burgers (
+CREATE TABLE burger (
 id int NOT NULL AUTO_INCREMENT,
 burger_name varchar(255) NOT NULL,
 devoured BOOL DEFAULT false,
